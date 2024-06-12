6f2df3dae163bcfdb32f1c56f431609b79e7711b
Joona Karhu <n1kajo01@students.oamk.fi>
2024-06-12 13:40:23 +0300

    tutorial level layout changes

--- Assets/Scripts/Input/KeyInputManager.cs
+++ Assets/Scripts/Input/KeyInputManager.cs
@@ -4,36 +4,28 @@ using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class KeyInputManager : MonoBehaviour
-{
+public class KeyInputManager : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
-        if (SceneManager.GetActiveScene().name == "Gameplay")
-        {
-            if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count == 0 && Player.instance.enabled == true)
-            {
+        if (SceneManager.GetActiveScene().name == "Gameplay") {
+            if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count == 0 && Player.instance.enabled == true) {
                 GameInputLogic.PlayerShowWindow(WindowPanel.PauseMenu);
             }
-            else if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count > 0 && Player.instance.enabled == true)
-            {
+            else if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count > 0 && Player.instance.enabled == true) {
                 // Call your method to close a specific window, e.g., WindowPanel.Inventory
                 GameInputLogic.PlayerCloseWindow(WindowManager.instance.escapeableWindowStack.Pop());
             }
-            else if (Input.GetKeyDown(KeyCode.Tab) && Player.instance.enabled == true)
-            {
+            else if (Input.GetKeyDown(KeyCode.Tab) && Player.instance.enabled == true) {
                 GameInputLogic.TabPressed();
             }
         }
-        else if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count > 0)
-        {
+        else if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count > 0) {
             GameInputLogic.PlayerCloseWindow(WindowManager.instance.escapeableWindowStack.Pop());
         }
 
