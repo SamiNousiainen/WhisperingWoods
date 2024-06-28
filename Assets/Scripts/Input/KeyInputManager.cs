7ecf021f9837835588083fc645efddee9306333e
Joona Karhu <n1kajo01@students.oamk.fi>
2024-06-28 12:36:17 +0300

    deleted unused scripts & added timescale for pausemenu

--- Assets/Scripts/Input/KeyInputManager.cs
+++ Assets/Scripts/Input/KeyInputManager.cs
@@ -11,17 +11,19 @@ public class KeyInputManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 
-		if (SceneManager.GetActiveScene().name != "MainMenu" || SceneManager.GetActiveScene().name != "LoadingScreen") {
+		if (SceneManager.GetActiveScene().name != "MainMenu" && SceneManager.GetActiveScene().name != "LoadingScreen") {
 
 			if (Player.instance != null && WindowManager.instance.escapeableWindowStack.Count == 0) {
+				Time.timeScale = 1.0f;
 				if (Input.GetKeyDown(KeyCode.K) == true || Input.GetButtonDown("Fire1")) {
 					GameInputLogic.PlayerAttack();
 				}
 				else if (Input.GetButtonDown("Jump")) {
 					GameInputLogic.PlayerJump();
 				}
+			} else if (Player.instance != null && WindowManager.instance.escapeableWindowStack.Count > 0) {
+				Time.timeScale = 0f;
 			}
-
 
 			if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count == 0 && Player.instance.enabled == true) {
 				GameInputLogic.PlayerShowWindow(WindowPanel.PauseMenu);
@@ -34,10 +36,10 @@ public class KeyInputManager : MonoBehaviour {
 				GameInputLogic.TabPressed();
 			}
 		}
-		else if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count > 0) {
-			GameInputLogic.PlayerCloseWindow(WindowManager.instance.escapeableWindowStack.Pop());
+		else if (SceneManager.GetActiveScene().name == "MainMenu") {
+			if (Input.GetKeyDown(KeyCode.Escape) && WindowManager.instance.escapeableWindowStack.Count > 0) {
+				GameInputLogic.PlayerCloseWindow(WindowManager.instance.escapeableWindowStack.Pop());
+			}
 		}
-
-
 	}
 }
