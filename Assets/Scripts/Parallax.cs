c4d54f5fee98fafab8f319377ff321e9e219d670
SamiNousiainen <n1nosa00@students.oamk.fi>
2024-08-21 13:10:48 +0300

    Update Parallax.cs

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,39 +1,41 @@
 using UnityEngine;
 
 public class Parallax : MonoBehaviour {
-    private float length;
-    private float startPosX;
-    private float startPosY;
-    public Camera cam;
-    public float parallaxEffectAmount;
+	private float length;
+	private float startPosX;
+	private float startPosY;
+	public Camera cam;
+	public float parallaxEffectAmount;
 
-    void Start() {
-        startPosX = transform.position.x;
-        startPosY = transform.position.y;
+	void Start() {
+		startPosX = transform.position.x;
+		startPosY = transform.position.y;
 
-        cam = Camera.main;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-    }
+		cam = Camera.main;
+		length = GetComponent<SpriteRenderer>().bounds.size.x;
+	}
 
 
-    void Update() {
-        float camDisplacementX = cam.transform.position.x * (1 - parallaxEffectAmount);
-        float camDisplacementY = cam.transform.position.y * (1 - parallaxEffectAmount);
-        float distanceX = cam.transform.position.x * parallaxEffectAmount;
-        float distanceY = cam.transform.position.y * parallaxEffectAmount;
+	void Update() {
+		float camDisplacementX = cam.transform.position.x * (1 - parallaxEffectAmount);
+		float camDisplacementY = cam.transform.position.y * (1 - parallaxEffectAmount);
+		float distanceX = cam.transform.position.x * parallaxEffectAmount;
+		float distanceY = cam.transform.position.y * parallaxEffectAmount;
 
-        transform.position = new Vector3(startPosX + distanceX, startPosY + distanceY, transform.position.z);
+		transform.position = new Vector2(startPosX + distanceX, startPosY + distanceY);
 
-        if (camDisplacementX > startPosX + length) {
-            startPosX += length;
-        } else if (camDisplacementX < startPosX - length) {
-            startPosX -= length;
-        }
+		if (camDisplacementX > startPosX + length) {
+			startPosX += length;
+		}
+		if (camDisplacementX < startPosX - length) {
+			startPosX -= length;
+		}
 
-        //if (camDisplacementY > startPosY + length) {
-        //    startPosY += length;
-        //} else if (camDisplacementY < startPosY - length) {
-        //    startPosY -= length;
-        //}
-    }
+		if (camDisplacementY > startPosY + length) {
+			startPosY += length;
+		}
+		if (camDisplacementY < startPosY - length) {
+			startPosY -= length;
+		}
+	}
 }
