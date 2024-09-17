1d4ed5524545eccb0debd785e286f53a5d40f00c
SamiNousiainen <n1nosa00@students.oamk.fi>
2024-09-17 18:48:23 +0300

    new boss attack WIP

--- Assets/Scripts/CrystalGhost.cs
+++ Assets/Scripts/CrystalGhost.cs
@@ -6,7 +6,11 @@ public class CrystalGhost : Enemy {
 
 	[SerializeField]
 	private GameObject lightningPrefab;
+	[SerializeField]
+	private GameObject crystalPrefab;
 	private List<GameObject> attacks;
+	private float burstAmount = 5f;
+	private float projectileInterval = 0.7f;
 
 	void Start() {
 		health = 500f;
@@ -18,13 +22,33 @@ public class CrystalGhost : Enemy {
 
 	public override void TakeDamage(float damage) {
 		base.TakeDamage(damage);
-		StartCoroutine(LightningAttack());
+		StartCoroutine(CrystalAttack());
+		//StartCoroutine(LightningAttack());
 	}
 
 	private IEnumerator LightningAttack() {
-		GameObject lightning = Instantiate(lightningPrefab, new Vector2(BossArena.instance.transform.position.x, 7), Quaternion.identity);
-		yield return new WaitForSeconds(2);
-		Destroy(lightning);
+		if (lightningPrefab != null) {
+			GameObject lightning = Instantiate(lightningPrefab, BossArena.instance.transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(2);
+			Destroy(lightning);
+		}
+	}
+
+	public void FallingCrystals() {
+		Vector2 crystalSpawnPos = new Vector2(Player.instance.transform.position.x, 23); //set Y pos instead of manual
+		if (crystalPrefab != null) {
+			Instantiate(crystalPrefab, crystalSpawnPos, Quaternion.identity);
+		}
+	}
+
+	private IEnumerator CrystalAttack() {
+		//attackTimer = attackTime;
+
+		for (int i = 0; i < burstAmount; i++) {
+			FallingCrystals();
+
+			yield return new WaitForSeconds(projectileInterval);
+		}
 	}
 
 	public override void DealDamage() {
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	float damage = 10f;
+	private Collider2D coll;
+	void Start() {
+		coll = GetComponent<Collider2D>();
+	}
+
+	void Update() {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Debug.Log("hit");
+		Player player = collision.gameObject.GetComponent<Player>();
+		if (player != null && Player.instance.damageCooldownTimer < 0f) {
+			Player.instance.TakeDamage(damage, transform);
+		}
+		Destroy(gameObject);
+	}
+}
